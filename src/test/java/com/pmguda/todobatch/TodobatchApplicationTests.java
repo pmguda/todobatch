8d7e24e1c2401f7f4a45c0c3721d8a0d881cbb2e
이성구 <ganziguda@gmail.com>
2022-03-24 15:50:21 +0900

    batch

---
+++ src/test/java/com/pmguda/todobatch/TodobatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmguda.todobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodobatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
